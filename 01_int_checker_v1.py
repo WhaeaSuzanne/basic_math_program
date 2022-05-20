e99d35c391187d177216dd86ad2a59c1f212d5a9
WhaeaSuzanne <101528367+WhaeaSuzanne@users.noreply.github.com>
2022-05-20 18:05:53 +1200

    Add files via upload

---
+++ 01_int_checker_v1.py
@@ -0,0 +1,46 @@
+'''
+game to ask basic math questions
+base component
+v1 - set up the seleton of the program
+CC SA 2022
+
+'''
+#import library************************************************
+import random as r
+
+#set up functions**********************************************
+def int_checker():
+    print("placeholder")
+
+def string_checker(list_of_allowable):
+    print("placeholder")
+
+def addition():
+    print("placeholder")
+
+def subtraction():
+    print("placeholder")
+
+def multiplication():
+    print("placeholder")
+
+def division():
+    print("placeholder")
+
+
+#set up lists and constants************************************
+answer_list = []
+yes_no_list = [["yes", "y"],["no","n"]]
+operands = ["+","-","*", "/"]
+
+
+#set up main***************************************************
+if __name__ == "__main__":
+    print("placeholder")
+    #ask users for input
+    print("*******************MATH GAME****************************")
+    print("**")
+    print("** Welcome to my Math Game ")
+    operand = input("** Choose an operand for your game - +, -, *, / ")
+    #check operand for legitimacy
+    #string check - send the list of operands
