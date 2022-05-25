3fe328ed217639b9fd34d39ef3924f96b233e1a8
WhaeaSuzanne <101528367+WhaeaSuzanne@users.noreply.github.com>
2022-05-26 11:39:17 +1200

    initial function created and tested.
    Function not completed, simply a test to make sure the function is returning a value

---
+++ 02_string_checker_v1.py
@@ -0,0 +1,16 @@
+'''
+v1 component string checker - check to make sure the funciton is called. This is working.
+
+'''
+def str_checker():
+
+    text_str = "placeholder"
+
+
+    return text_str
+
+
+# **main
+yes_no = str_checker()
+print (yes_no)
+
