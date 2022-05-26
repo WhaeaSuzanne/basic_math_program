b09cefcd4551ad47acf0f64aa79a0b934304ce0b
WhaeaSuzanne <101528367+WhaeaSuzanne@users.noreply.github.com>
2022-05-26 12:30:13 +1200

    add str_checker function to base component
    add string checker function to base component. Works for yes_no.
    Next up, add into base component for checking the operand

---
+++ 00_base_v4.py
@@ -0,0 +1,143 @@
+'''
+game to ask basic math questions
+base component
+v1 - set up the skeleton of the program, test the functions are called as required
+v2 - trial random generator function component. Works well so will keep it
+V3 - add int checker function component to the base component - tested and running, trialled the questions for low and high num
+v4 - add string checker function to base component. Works for yes_no.
+CC SA 2022
+
+'''
+#import library************************************************
+import random as r
+
+#set up functions**********************************************
+
+#function to check for integers. will loop until an integer is input
+#checked this and is working @25/5/22
+def int_checker(question, error_message):
+    x = True
+    while x == True:
+
+        try:
+            int_num = int(input(question))
+            x = False
+        except:
+            print(error_message)
+
+    return(int_num)
+
+
+#function to check string is within an allowable list. Will loop until an allowable word is input
+#str checker function - checks a string input is within a list of acceptable answers, returns the answer.
+def str_checker(gen_lst, err_msg, choice):
+    is_valid = ""
+    chosen = ""
+
+    for option in gen_lst:
+        if choice in option:
+            chosen = option[0]
+            break
+        else:
+            chosen = "not_valid"
+
+    return chosen
+
+
+#function to add two randomly generated numbers
+def addition():
+    print("addition function")
+
+#function to subtract two randomly generated numbers
+def subtraction():
+    print("subtraction function")
+
+#function to multiply two randomly generated numbers
+def multiplication():
+    print("multiplication function")
+
+#function to divide two randomly generated numbers
+def division():
+    print("division")
+
+#trialling random generator as part of v1 base component
+def random_generator(num_1, num_2):
+    rand_num = r.randint(num_1, num_2)
+    return rand_num
+#random generator component works well.
+
+
+
+#set up lists and constants************************************
+answer_list = []
+yes_no_lst = [["yes", "y"],["no","n"]]
+operands = ["+","-","*", "/", "all"]
+
+
+#set up main***************************************************
+if __name__ == "__main__":
+    #set local variables within the main routine
+    min_num = 0
+    max_num = 5
+
+    print("placeholder")
+    #ask users for input
+    print("*******************MATH GAME****************************")
+    print("**")
+    print("** Welcome to my Math Game ")
+    operand = input("** Choose an operand for your game - +, -, *, / or all: ")
+    #check operand for legitimacy
+    #string check - send the list of operands
+
+    #ask user if they wish to use negatives?
+    #yes/no list and answer sent to string checker
+    #loop input until the answer is returned as positive
+    #move to yes_no area
+    yes_no_err_msg = "Please enter yes or no."
+    negatives_ques = "Do you want to include negatives in your math questions? Enter either yes or no: "
+
+    valid = True
+    while valid == True:
+        answer = input(negatives_ques)
+        answer = answer.lower()
+        answer = str_checker(yes_no_lst, yes_no_err_msg, answer)
+        if answer == "yes" or answer == "no":
+            valid = False
+        else:
+            print("This is not a valid answer. Please try again.")
+    print(answer)
+
+    #ask user for low and high numbers
+    #int checker to check its an integer, loop until suitable integer
+    low_num = int_checker("Please enter a low number: ", "This is not an integer - please enter an integer: ")
+    print(low_num)
+    high_num = int_checker("Please enter a high number: ", "This is not an integer - please enter an integer: ")
+    print(high_num)
+
+    #ask user for number of questions to be asked
+    #int checker to check on the integer
+    num_questions = 1
+
+    #loop until number of questions is asked
+    for questions in range(0, num_questions):
+        #print("placeholder")
+        if operand == "+":
+            #print("go to addition")
+            addition()
+        elif operand == "-":
+            #print("got to subtraction")
+            subtraction()
+        elif operand == "*":
+            #print("go to multiplication")
+            multiplication()
+        elif operand == "/":
+            #print("go to division")
+            division()
+        else:
+            #this is called when user inputs all
+            min_num = 1
+            max_num = 5
+            random_num = random_generator(min_num, max_num)
+            print(random_num)
+            #print("go to random generator, send min_num and max_num (1 and 5)")
+            #print("then randomly go to each of the operand functions")
